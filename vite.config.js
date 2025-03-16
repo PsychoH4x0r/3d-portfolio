6760a4ed995a41cadc6db8374573a500f7f01614
Unknown1337 <88354254+PsychoH4x0r@users.noreply.github.com>
2025-03-16 21:04:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/3d-portfolio/",  // Sesuai dengan repo GitHub Pages lo
+  base: "/3d-portfolio/",  // Pastikan ini sesuai dengan nama repo GitHub Pages lo
   plugins: [react()],
 });
